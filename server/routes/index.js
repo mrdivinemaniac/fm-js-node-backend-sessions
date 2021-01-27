33c172a1e877cc51187b7b787f8545a2cca933d2
mrdivinemaniac <divinemaniac@gmail.com>
2021-01-28 01:34:48 +0545

    Refactor upload route - Rename /uploads to /posts - Create route for stream example

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,34 +1,74 @@
 const express = require('express')
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
+
+class InvalidFileError extends Error {}
 
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  // TODO: pass variables
+  res.render('index')
+})
 
 const storage = multer.diskStorage({
   destination: './public/uploads',
   filename: (req, file, cb) => {
-      // TODO: use UUID
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    // TODO: use UUID
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
 })
 
-const upload = multer({ storage }).single('testFile')
-
-router.get('/', (req, res) => {
-    // TODO: pass variables
-    res.render('index')
+// Check error handler in the bottom
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    // accept only text files
+    const fileIsValid = (
+      file.mimetype.startsWith('text/') ||
+      ['application/javascript', 'application/json'].includes(file.mimetype)
+    )
+    if (!fileIsValid) cb(new InvalidFileError('Uploaded file must be a text file'))
+    else cb(null, true)
+  }
 })
 
-// TODO: use middleware
-router.post('/uploads', (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      res.send(err)
-    } else {
-      console.log({ file: req.file})
-      res.send('test')
-    }
+// Jagaran
+router.post('/posts', upload.single('codeFile'), (req, res) => {
+  console.log('Received posts request')
+  if (!req.body.message) {
+    res.status(400).send({ error: 'message is required' })
+    return
+  }
+  res.json({
+    id: Date.now(), // TODO: use uuid
+    message: req.body.message,
+    codeFilePath: req.file.filename,
+    likes: 0
   })
+})
+
+// Bikash
+router.post('/stream-upload', (req, res) => {
+  // Uncomment to show data coming in
+  // req.on('data', data => {
+  //   console.log('---data', data.toString())
+  // })
+  const fileStream = fs.createWriteStream('./testwrite.txt')
+  req.pipe(fileStream)
+  req.on('end', () => {
+    res.send('ok!')
+  })
+})
+
+// Error handler
+router.use((err, req, res, next) => {
+  if (err instanceof InvalidFileError) {
+    res.status(400).send({ error: err.message })
+  } else {
+    next(err)
+  }
 })
 
 module.exports = router
