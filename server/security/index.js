a5a5e0c56e4c6acfeb63eb678671f8d0ebc8b02d
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-03 10:11:59 +0545

    Create token helper functions

---
+++ server/security/index.js
@@ -0,0 +1,57 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET , JWT_ISSUER} = require('../config')
+
+const JWT_ALGORITHM = 'HS256'
+
+function signAuthToken (user, scopes) {
+  return signToken({
+    userId: user.id,
+    scopes: Array.isArray(scopes) ? scopes : [scopes]
+  })
+}
+
+function signToken (payload, expiresIn) {
+  return new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      JWT_SECRET,
+      createJwtOptions(expiresIn),
+      (err, token) => {
+        if (err) reject(err)
+        else resolve(token)
+      }
+    )
+  })
+}
+
+function verifyToken (token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      JWT_SECRET,
+      {
+        algorithms: [JWT_ALGORITHM],
+        issuer: [JWT_ISSUER]
+      },
+      (err, decoded) => {
+        if (err) reject(err)
+        else resolve(decoded)
+      }
+    )
+  })
+}
+
+function createJwtOptions (expiresIn) {
+  const options = {
+    algorithm: JWT_ALGORITHM,
+    issuer: JWT_ISSUER
+  }
+  if (expiresIn) options.expiresIn = expiresIn
+  return options
+}
+
+module.exports = {
+  signAuthToken,
+  signToken,
+  verifyToken
+}
