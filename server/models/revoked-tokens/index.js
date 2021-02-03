cdcc8a6a0c00f21b9f8784b5afa0717e30d13acc
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-03 10:47:02 +0545

    Add mock models for users and revoked tokens

---
+++ server/models/revoked-tokens/index.js
@@ -0,0 +1,10 @@
+/**
+ * Revokes a token issued by the server
+ */
+function revokeToken (token, reason) {
+  // TODO: implement after database integration
+}
+
+module.exports = {
+  revokeToken
+}
