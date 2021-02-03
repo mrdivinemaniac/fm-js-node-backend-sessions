3b50718d54f374a6a762309d5538c3b344376a19
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-03 10:53:13 +0545

    Add /auth/token and /auth/revoke routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const postRouter = require('./posts')
+const authRouter = require('./auth')
 
 const router = express.Router()
 
 router.use('/posts', postRouter)
+router.use('/auth', authRouter)
 
 module.exports = router
