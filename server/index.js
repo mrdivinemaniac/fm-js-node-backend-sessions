3dce79bb32965274c6a5cff1dcedecb17059ee2d
jaggs07 <jagaran@fusemachines.com>
2021-02-03 10:43:17 +0545

    Add support for json parse

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const config = require('./config')
 const routes = require('./routes')
+// const bodyParser = require('body-parser')
 
 const app = express()
+
+// express json support from version 4.16.0
+app.use(express.json())
+
+// using body parser
+// app.use(bodyParser.json())
 
 app.use('/', routes)
 
