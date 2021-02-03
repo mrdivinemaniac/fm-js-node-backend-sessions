e9e745a4be215495e64f5e61d0020a3f8b569ff0
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-03 10:16:42 +0545

    Add auth routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const postRouter = require('./posts')
+const authRouter = require('./auth')
+const { checkAuth } = require('../middleware')
 
 const router = express.Router()
 
-router.use('/posts', postRouter)
+router.use('/posts', checkAuth(), postRouter)
+router.use('/auth', authRouter)
 
 module.exports = router
