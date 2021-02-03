9681739571691ac9e37c31640ed408fef3185c94
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-03 10:58:23 +0545

    Add skeleton for token utils in cli

--- cli/utils/index.js
+++ cli/utils/index.js
@@ -3,6 +3,20 @@ const path = require('path')
 const fetch = require('node-fetch')
 const config = require('../config')
 
+const DATA_FILE_PATH = path.join(__dirname, '../', 'data')
+
+function saveToken (token) {
+  
+}
+
+function readToken () {
+  
+}
+
+function deleteToken () {
+  
+}
+
 function requestApi (apiPath, ...rest) {
   return fetch(`${config.apiHost}/${apiPath}`, ...rest)
 }
