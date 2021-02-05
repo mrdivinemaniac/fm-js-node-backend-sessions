806caaa9b9a0715f34986d4dbb338dd1bdea34cd
jaggs07 <jagaran@fusemachines.com>
2021-02-05 13:25:39 +0545

    Implement routes
    - GET posts
    - GET posts/:postId
    - DELETE posts/:postId
    - PATCH posts/:postId

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -44,33 +44,54 @@ router.post('/', upload.single('codeFile'), async (req, res, next) => {
   }
 })
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    
-  } catch (e) {
-    next(e)
-  }
-})
-
+// Get all lists 
 router.get('/', async (req, res, next) => {
   try {
-
+    const posts = await postsModel.getPosts()
+    return res.json(posts)
   } catch (e) {
     next(e)
   }
 })
 
-router.delete('/:id', async (req, res, next) => {
+router.param('postId', async (req, res, next, postId) => {
   try {
-    
+    const post = await postsModel.getPost(postId)
+    if (!post) {
+      res.status(404).send('Post not found')
+    } else {
+      req.post = post
+      next()
+    }
   } catch (e) {
     next(e)
   }
 })
 
-router.patch('/:id', upload.single('codeFile'), async (req, res, next) => {
-  try {
+// Getting a single post by id
+router.get('/:postId', async (req, res, next) => {
+  res.json(req.post)
+})
 
+router.delete('/:postId', async (req, res, next) => {
+  try {
+    const { postId } = req.params
+    await postsModel.deletePost(postId)
+    res.send()
+  } catch (e) {
+    next(e)
+  }
+})
+
+// updating certain fields in the record
+router.patch('/:postId', upload.single('codeFile'), async (req, res, next) => {
+  try {
+    const { postId } = req.params
+    const { message } = req.body
+    const file = req.file
+
+    const updatedPost = await postsModel.updatePost(postId, message, file)
+    res.json(updatedPost)
   } catch (e) {
     next(e)
   }
