58bb8acda39b48aaccc9dba77bd61e3ee92eb63d
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-16 14:05:33 +0545

    Add global error handler

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const app = express()
 
 app.use('/', routes)
 
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: 'Unknown error occurred' })
+})
+
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
