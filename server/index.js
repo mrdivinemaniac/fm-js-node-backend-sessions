589201755c49e654babf880e68a1591eee9ced9b
mrdivinemaniac <divinemaniac@gmail.com>
2021-02-16 14:39:38 +0545

    Handle uncaught exceptions and rejections

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const config = require('./config')
 const routes = require('./routes')
+const { createLogger } = require('./logger')
+
+const logger = createLogger('index')
 
 const app = express()
 
@@ -10,6 +13,16 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Unknown error occurred' })
 })
 
-app.listen(config.PORT, () => {
+const server = app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
+})
+
+process.on('uncaughtException', err => {
+  logger.error('Uncaught Exception!', err)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', err => {
+  logger.error('Unhandled Rejection!', err)
+  process.exit(1)
 })
