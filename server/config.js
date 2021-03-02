b5a56ed02b48cee51364a382a137e4a2080b7960
mrdivinemaniac <divinemaniac@gmail.com>
2021-03-02 21:13:37 +0545

    Get DB_URL from env

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET || 'ZDbC5g1x7y1Vgo3VTiUUYzenbWDWjZzr', // Generated using https://randomkeygen.com/
   JWT_ISSUER: process.env.JWT_ISSUER || 'developer-social/auth-local',
-  DB_URL: 'postgres://:bf123456@localhost/dev-social'
+  DB_URL: process.env.DB_URL || 'postgres://:bf123456@localhost/dev-social'
 }
