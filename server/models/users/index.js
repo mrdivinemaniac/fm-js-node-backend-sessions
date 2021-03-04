e52974512cb4b5a16bc9e47647c64946ebd24109
mrdivinemaniac <divinemaniac@gmail.com>
2021-03-04 12:14:15 +0545

    Implement queries for login and signup

--- server/models/users/index.js
+++ server/models/users/index.js
@@ -1,28 +1,47 @@
 const uuid = require('uuid')
+const crypto = require('crypto')
+const { PASSWORD_SALT } = require('../../config')
+const { dbClient } = require('../../database')
+
+class UserAlreadyExistsError extends Error {}
 
 /**
  * Checks if the user with the provided credentials exists
  * and returns the user object
  */
-function findUserByCredentials (username, password) {
-  // TODO: implement after database integration
-  return Promise.resolve({
-    id: uuid.v4(),
-    username
-  })
+async function findUserByCredentials (username, password) {
+  const userByUsername = await dbClient('users').select('*').where({ username })
+  return (userByUsername.password_hash === hashPassword(password))
+    ? username
+    : null
 }
 
-function createUser (email, username, password) {
-  // TODO: Insert into Database
-  return Promise.resolve({
-    id: uuid.v4(),
-    email,
-    username,
-    password
-  })
+async function createUser (email, username, password) {
+  const user = await dbClient('users')
+    .select('id')
+    .where({ email })
+    .orWhere({ username })
+    .first()
+  if (user) throw new UserAlreadyExistsError()
+  return dbClient('users')
+    .insert({
+      id: uuid.v4(),
+      email,
+      username,
+      password_hash: hashPassword(username, password)
+    })
+}
+
+function hashPassword (username, password) {
+  return crypto.createHash('sha256')
+    .update(password)
+    .update(username)
+    .update(PASSWORD_SALT)
+    .digest('hex')
 }
 
 module.exports = {
   findUserByCredentials,
-  createUser
+  createUser,
+  UserAlreadyExistsError
 }
